8fb913e29c1fe457009a6639760fc11a58a69b35
pch6828 <jeongho.park@kaist.ac.kr>
2023-06-29 22:13:20 +0900

    bug fix for data load script

--- tpcds_load_each_table.sh
+++ tpcds_load_each_table.sh
@@ -9,23 +9,42 @@ DATA_PATH=/data/pch
 
 cd $DATA_PATH/tpc-ds/data/$SCALE
 
-for i in $(seq $START_CHUNK $END_CHUNK)
-do
-    FILENAME=${TABLE}_${i}_${SCALE}.dat
-    if [ -f "$FILENAME" ]; then
-        linecount=$(wc --lines < $FILENAME)
+if [ $SCALE -eq 1 ]
+then
+    echo "Processing ${TABLE}"
+    if [ -f "$TABLE.dat" ]; then
+        linecount=$(wc --lines < $TABLE.dat)
         if [ $linecount = 0 ];then
-            echo "$FILENAME is not generated properly"
+            echo "$TABLE.dat is not generated properly"
             break
         fi        
-        echo "Processing Chunk ${i}"
-        python3 $SETUP_PATH/fix-utf8.py $FILENAME ${FILENAME%.*}_2.dat
-        psql $DB_NAME -c "\\copy ${TABLE} FROM '${FILENAME%.*}_2.dat' CSV DELIMITER '|'" 
-        rm $FILENAME    
-    elif [ -f "${FILENAME%.*}_2.dat" ]; then          
-        echo "Processing Chunk ${i}"          
-        psql $DB_NAME -c "\\copy ${TABLE} FROM '${FILENAME%.*}_2.dat' CSV DELIMITER '|'" 
-    else
-        break
+        python3 $SETUP_PATH/fix-utf8.py $TABLE.dat ${TABLE}_2.dat
+        psql $DB_NAME -c "\\copy ${TABLE} FROM '${TABLE}_2.dat' CSV DELIMITER '|'" 
+        rm $TABLE.dat
+    elif [ -f "${TABLE}_2.dat" ]; then           
+        psql $DB_NAME -c "\\copy ${TABLE} FROM '${TABLE}_2.dat' CSV DELIMITER '|'"    
     fi
-done            
+else
+    for i in $(seq $START_CHUNK $END_CHUNK)
+    do
+        FILENAME=${TABLE}_${i}_${SCALE}.dat
+        if [ -f "$FILENAME" ]; then
+            linecount=$(wc --lines < $FILENAME)
+            if [ $linecount = 0 ];then
+                echo "$FILENAME is not generated properly"
+                break
+            fi        
+            echo "Processing Chunk ${i}"
+            python3 $SETUP_PATH/fix-utf8.py $FILENAME ${FILENAME%.*}_2.dat
+            psql $DB_NAME -c "\\copy ${TABLE} FROM '${FILENAME%.*}_2.dat' CSV DELIMITER '|'" 
+            rm $FILENAME    
+        elif [ -f "${FILENAME%.*}_2.dat" ]; then          
+            echo "Processing Chunk ${i}"          
+            psql $DB_NAME -c "\\copy ${TABLE} FROM '${FILENAME%.*}_2.dat' CSV DELIMITER '|'" 
+        else
+            break
+        fi
+    done       
+fi
+
+     
